1bfcfd3ff40b8d147db3aad6016c5795a09e535a
isaac chiang <96643832+Isaacjiang1982@users.noreply.github.com>
2026-02-24 07:43:03 +0800

    Add files via upload

--- events_data.js
+++ events_data.js
@@ -6,18 +6,18 @@ window.specialEvents = [
     "description": "佛陀直播开示" 
   },
   { 
-    "date": "2026-03-10", 
+    "date": "待定", 
     "type": "dharma_assembly", 
     "description": "药师忏法会" 
   },
   { 
-    "date": "2026-03-20", 
+    "date": "待定", 
     "type": "retreat", 
     "description": "精进佛七" 
   },
   { 
-    "date": "2026-04-15", 
+    "date": "待定", 
     "type": "organization_conference", 
-    "description": "年度组织大会" 
+    "description": "组织大会" 
   }
 ];
