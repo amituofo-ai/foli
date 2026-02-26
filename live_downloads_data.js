67d1ebd360e9c9db815db8f2538e08946a1a88eb
isaac chiang <96643832+Isaacjiang1982@users.noreply.github.com>
2026-02-26 20:49:29 +0800

    Add files via upload

---
+++ live_downloads_data.js
@@ -0,0 +1,11 @@
+
+const live_downloads = [
+    {
+        "title": "《阿弥陀佛圣诞日全球直播20260105（农历11月17）》",
+        "url": "https://mypikpak.com/s/VOmO8gBp8ETcYaX_oJeuu9zCo2"
+    },
+    {
+        "title": "《2026年2月16除夕夜直播》",
+        "url": "https://mypikpak.com/s/VOmO8l46a9Rki_i5xbHvwQao2"
+    }
+];
