8be1e7a2db956263574e7703e11d75765e8e5ffa
isaacCHI <96643832+amituofo-ai@users.noreply.github.com>
2026-02-28 22:53:02 +0800

    Add files via upload

--- live_downloads_data.js
+++ live_downloads_data.js
@@ -7,5 +7,17 @@ const live_downloads = [
     {
         "title": "20260105 阿彌陀佛聖誕全球直播開示",
         "url": "https://www.alipan.com/s/8opuDH9H9qv"
+    },
+    {
+        "title": "20251108 觀音菩薩吉祥日直播",
+        "url": "https://www.alipan.com/s/Zt6JyZShYHv"
+    },
+    {
+        "title": "20250505 釋迦佛聖誕日直播",
+        "url": "https://www.alipan.com/s/K8fAgqi9okV"
+    },
+    {
+        "title": "20250501 文殊菩薩聖誕日直播",
+        "url": "https://www.alipan.com/s/WDNSPMkibCw"
     }
 ];
