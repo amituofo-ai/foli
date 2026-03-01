d8cec3cafb625989d97f93ed9a5a9f8291b73b92
isaacCHI <96643832+amituofo-ai@users.noreply.github.com>
2026-03-01 15:13:24 +0800

    Add files via upload

--- live_downloads_data.js
+++ live_downloads_data.js
@@ -1,23 +1,33 @@
 
 const live_downloads = [
     {
-        "title": "20260216 除夕夜全球直播開示",
-        "url": "https://www.alipan.com/s/AGykZhEuhWq"
+        "title": "20260216 除夕夜全球直播",
+        "url1": "https://www.alipan.com/s/AGykZhEuhWq",
+        "url2": "https://e.pcloud.link/publink/show?code=XZFmq3ZapSQhhr0O40Ko7uvF4dNyBxzfWpy"
     },
     {
-        "title": "20260105 阿彌陀佛聖誕全球直播開示",
-        "url": "https://www.alipan.com/s/8opuDH9H9qv"
+        "title": "20260105 阿彌陀佛聖誕日直播",
+        "url1": "https://www.alipan.com/s/8opuDH9H9qv",
+        "url2": "https://e.pcloud.link/publink/show?code=XZ5mq3Zr0JdthwcwFkjkfaiml5i3fTBzk9k"
     },
     {
         "title": "20251108 觀音菩薩吉祥日直播",
-        "url": "https://www.alipan.com/s/Zt6JyZShYHv"
+        "url1": "https://www.alipan.com/s/Zt6JyZShYHv",
+        "url2": "https://e.pcloud.link/publink/show?code=XZ7mq3ZGek5S2K2VtHDY6Lhqg335hp2Eh6V"
     },
     {
         "title": "20250505 釋迦佛聖誕日直播",
-        "url": "https://www.alipan.com/s/K8fAgqi9okV"
+        "url1": "https://www.alipan.com/s/K8fAgqi9okV",
+        "url2": "https://e.pcloud.link/publink/show?code=XZoYq3ZSjxqPlvWsRb48dG4PM34XHap56Gy"
     },
     {
         "title": "20250501 文殊菩薩聖誕日直播",
-        "url": "https://www.alipan.com/s/WDNSPMkibCw"
+        "url1": "https://www.alipan.com/s/WDNSPMkibCw",
+        "url2": "https://e.pcloud.link/publink/show?code=XZvYq3ZcF32YE9t625dIkWD2g91QSmWFQHX"
+    },
+    {
+        "title": "20250318 觀音菩薩吉祥日直播",
+        "url1": "https://www.alipan.com/s/SAHrw1onFf5",
+        "url2": "https://e.pcloud.link/publink/show?code=XZaYq3ZlW5UGPBctakIrmjNsoO33kYEzvMk"
     }
 ];
